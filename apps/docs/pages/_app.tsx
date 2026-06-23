e7ab01b4aa9a8c2bdf7be0051ed17b34b2bcd5a8
didirb <irbayedubadida@gmail.com>
2026-06-23 18:12:20 +0300

    feat(docs): rebuild Nextra v3 from scratch with standalone landing page and library-aware structure

---
+++ apps/docs/pages/_app.tsx
@@ -0,0 +1,6 @@
+import type { ReactElement } from 'react'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />
+}
